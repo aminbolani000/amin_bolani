2672a085bc4216acf2041cc51f1a1ee1f5529400
aminbolani000 <aminbhaioffical@gmail.com>
2026-09-07 21:22:11 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-console.log("M.Suhaib Watches Website Loaded Successfully!");
-
-// Chota sa interaction test karne ke liye
-const shopBtn = document.getElementById("shop-btn");
-shopBtn.addEventListener("click", () => {
-    alert("Welcome! Scroll down to see our watch collections.");
-});
